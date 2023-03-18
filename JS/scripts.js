4a0a1b648f81ddeda8e03d5c5822bdce7b7bf890
Sarp Yilmaz <121580209+S4RP@users.noreply.github.com>
2023-03-18 17:32:48 +0300

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,48 @@
+// Direct user to requested pages
+function direct_home()
+{
+    location.href = '/index';
+}
+
+function direct_bio()
+{
+    location.href = '/bio';
+}
+
+function direct_lib()
+{
+    location.href = '/library';
+}
+
+function direct_adv()
+{
+    location.href = '/adventures';
+}
+
+//Google Search
+function google_search()
+{
+    // Get the user answer
+    let input = document.querySelector('#gsearch').value;
+    location.href = 'https://www.google.com/search?q=' + input;
+    target="_blank";
+}
+
+// Wait for DOM to load before selecting anything
+document.addEventListener('DOMContentLoaded', function(){
+    // Direct to home page
+    document.querySelector('#homebut').addEventListener('click', direct_home);
+    // Direct to bio page
+    document.querySelector('#biobut').addEventListener('click', direct_bio);
+    // Direct to library page
+    document.querySelector('#libbut').addEventListener('click', direct_lib);
+    // Direct to adventure page
+    document.querySelector('#advbut').addEventListener('click', direct_adv);
+    // Google Search
+    document.querySelector('#googlesearch').addEventListener('submit', google_search);
+
+    window.initMap = initMap;
+})
+
+
+
